e5cc58f2f39602ad96f8b006b849db9f3a548d7e
Alex Luu <alex__luu@hotmail.com>
2022-08-13 00:16:35 -0500

    Passport Facebook

--- authenticate.js
+++ authenticate.js
@@ -8,6 +8,8 @@ const jwt = require("jsonwebtoken"); // used to create, sign, and verify tokens
 
 const config = require("./config.js");
 
+const FacebookTokenStrategy = require("passport-facebook-token");
+
 exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
@@ -33,6 +35,37 @@ exports.jwtPassport = passport.use(
       }
     });
   })
+);
+
+exports.facebookPassport = passport.use(
+  new FacebookTokenStrategy(
+    {
+      clientID: config.facebook.clientId,
+      clientSecret: config.facebook.clientSecret,
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ facebookId: profile.id }, (err, user) => {
+        if (err) {
+          return done(err, false);
+        }
+        if (!err && user) {
+          return done(null, user);
+        } else {
+          user = new User({ username: profile.displayName });
+          user.facebookId = profile.id;
+          user.firstname = profile.name.givenName;
+          user.lastname = profile.name.familyName;
+          user.save((err, user) => {
+            if (err) {
+              return done(err, false);
+            } else {
+              return done(null, user);
+            }
+          });
+        }
+      });
+    }
+  )
 );
 
 const verifyAdmin = (req, res, next) => {
